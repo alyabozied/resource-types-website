896bcf7ada62fc2442def54599a5bcaa7658995f
alyabozied <abozied@DESKTOP-DGRLDCK.localdomain>
2021-09-05 21:23:49 +0200

    test

--- production.sh
+++ production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # go version
 # go help modules
-export GH_TOKEN ='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'
+export GH_TOKEN="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"
 cd website_code/warehouse
 go run main.go
